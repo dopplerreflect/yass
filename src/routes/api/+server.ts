5368f5d568a06c6a7a52d35cdb6f8ed1cfb1c17f
David Rose <doppler@gmail.com>
2025-06-16 12:46:11 -0500

    formatting

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -6,7 +6,9 @@ import { runCommand } from '$lib/server/runCommand';
 export const POST: RequestHandler = async ({ request }) => {
 	const response: SerializedSvg = await request.json();
 	const { name, body } = response;
-	const saved = await saveFile(`./static/drawings/svg/${name}.svg`, body);
-	const { stdout, stderr } = await runCommand(`magick ./static/drawings/svg/${name}.svg -gravity center -crop '%[fx:min(w,h)]x%[fx:min(w,h)]+0x0' -resize 512x ./static/drawings/webp/${name}.webp`);
+	await saveFile(`./static/drawings/svg/${name}.svg`, body);
+	await runCommand(
+		`magick ./static/drawings/svg/${name}.svg -gravity center -crop '%[fx:min(w,h)]x%[fx:min(w,h)]+0x0' -resize 512x ./static/drawings/webp/${name}.webp`
+	);
 	return new Response(JSON.stringify('whatever'));
 };
--- src/routes/drawing/alchemy/setup.ts
+++ src/routes/drawing/alchemy/setup.ts
@@ -1,10 +1,10 @@
-import { anglesArray, phi } from "@dopplerreflect/geometry";
-import { createLines, linesExtendedToEdge } from "./lines";
+import { anglesArray, phi } from '@dopplerreflect/geometry';
+import { createLines, linesExtendedToEdge } from './lines';
 import { getCircles } from './circles';
 
 export const width = 1920;
-export const height = 1920;
-export const r = (height / 2) / Math.sqrt(3) * 0.75 ;
+export const height = 1080;
+export const r = (height / 2 / Math.sqrt(3)) * 0.75;
 export const radii = [...Array(4).keys()].map((n) => r * phi ** n);
 export const angles = anglesArray(6);
 
