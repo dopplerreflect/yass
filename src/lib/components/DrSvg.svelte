84f7d84b9b86c4f86bb091a7b7c6341bbb7b417b
David Rose <doppler@gmail.com>
2025-06-26 15:03:30 -0500

    add zooming

--- src/lib/components/DrSvg.svelte
+++ src/lib/components/DrSvg.svelte
@@ -21,18 +21,53 @@
 			}
 		});
 	}
+
+	const zoom = $state({
+		level: 1,
+		xOffset: 0,
+		yOffset: 0
+	});
+
+	function increment() {
+		if (zoom.level >= 0.2) zoom.level -= 0.1;
+	}
+
+	function decrement() {
+		if (zoom.level <= 0.9) zoom.level += 0.1;
+	}
+
+	function reset() {
+		zoom.level = 1;
+	}
+
+	function handleKey(event: KeyboardEvent) {
+		event.preventDefault();
+		switch (event.key) {
+			case '+':
+				increment();
+				break;
+			case '-':
+				decrement();
+				break;
+			case 'r':
+				reset();
+				break;
+		}
+	}
 	onMount(() => {
 		const pathname = document.location.pathname.split(/\//);
 		const name = pathname[pathname.length - 1];
 		const body = new XMLSerializer().serializeToString(svg);
 		postSVG({ name, body });
+		document.addEventListener('keypress', handleKey);
+		return () => document.removeEventListener('keypress', handleKey);
 	});
 </script>
 
 <svg
 	bind:this={svg}
 	xmlns="http://www.w3.org/2000/svg"
-	viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
+	viewBox={`${(-width / 2) * zoom.level} ${(-height / 2) * zoom.level} ${width * zoom.level} ${height * zoom.level}`}
 	color-interpolation-filters="sRGB"
 >
 	<mask id="drsvg-clipped">
