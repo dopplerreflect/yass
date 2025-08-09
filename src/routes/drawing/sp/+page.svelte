f709b7389207f49213b0ab7d53fbbfd8bf374e79
David Rose <doppler@gmail.com>
2025-08-09 11:38:55 -0500

    add sp

---
+++ src/routes/drawing/sp/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import DrSvg from '$lib/components/DrSvg.svelte';
+	const width = 1920;
+	const height = 1080;
+	const size = 50;
+	const r = size / 2;
+	const r1 = (Math.hypot(size, size) - size) / 2;
+</script>
+
+<DrSvg {...{ width, height }}>
+	<defs>
+		<pattern
+			id="sp"
+			width={size}
+			height={size}
+			viewBox={`${-size / 2} ${-size / 2} ${size} ${size}`}
+			patternUnits="userSpaceOnUse"
+		>
+			<g fill="white">
+				<circle {r} />
+				<circle r={r1} cx={-size / 2} cy={-size / 2} />
+				<circle r={r1} cx={size / 2} cy={-size / 2} />
+				<circle r={r1} cx={size / 2} cy={size / 2} />
+				<circle r={r1} cx={-size / 2} cy={size / 2} />
+			</g>
+		</pattern>
+	</defs>
+	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="black" />
+
+	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="url(#sp)" />
+</DrSvg>
