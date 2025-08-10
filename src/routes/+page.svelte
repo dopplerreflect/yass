f2457084f937140118eac879a297ff429c77664f
David Rose <doppler@gmail.com>
2025-08-09 22:48:26 -0500

    make thumbnails 256px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,13 @@
 			class="flex items-center justify-center rounded-2xl border-2 border-violet-700 bg-violet-950 p-2 drop-shadow-lg drop-shadow-violet-700 hover:drop-shadow-violet-300"
 		>
 			<a href={`/drawing/${drawing.name}`}>
-				<img class="rounded-2xl" src={`/drawings/webp/${drawing.name}.webp`} alt="drawing" />
+				<img
+					class="rounded-2xl"
+					width={256}
+					height={256}
+					src={`/drawings/webp/${drawing.name}.webp`}
+					alt="drawing"
+				/>
 				{drawing.name}
 			</a>
 		</div>
