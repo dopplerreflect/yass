ee314b357ad877f871a63ff8e6e2c8d7dba2fb73
David Rose <doppler@gmail.com>
2025-08-14 21:34:19 -0500

    hp2 change bg to black

--- src/routes/drawing/hp2/+page.svelte
+++ src/routes/drawing/hp2/+page.svelte
@@ -4,13 +4,13 @@
 	import themes from './themes';
 	const width = 1920;
 	const height = 1080;
-	const theme = themes.e;
+	const theme = themes.a;
 </script>
 
 <DrSvg {...{ width, height }}>
 	<defs>
 		<GoldenHexCirclePattern id="hp" hexRadius={height / 18} {theme} />
 	</defs>
-	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="white" />
+	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="black" />
 	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="url(#hp)" />
 </DrSvg>
