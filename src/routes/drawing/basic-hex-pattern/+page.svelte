d6ea0b628162b4001f1321b794ed292d046aa4de
David Rose <doppler@gmail.com>
2026-07-12 23:40:24 -0500

    bhp: purply dots

--- src/routes/drawing/basic-hex-pattern/+page.svelte
+++ src/routes/drawing/basic-hex-pattern/+page.svelte
@@ -7,21 +7,21 @@
 	import chroma from 'chroma-js';
 	const width = 1920;
 	const height = 1080;
-	const r = 50;
+	const r = 1920 / 55;
 </script>
 
 <DrSvg {...{ width, height }}>
 	<defs>
 		<filter id="bhp-filter" x="-50%" y="-50%" width="300%" height="300%">
-			<feMorphology in="SourceGraphic" operator="erode" radius={r * phi ** 6} />
-			<feGaussianBlur stdDeviation={r * phi ** 6} />
+			<feMorphology in="SourceGraphic" operator="erode" radius={r * phi ** 3} />
+			<feGaussianBlur stdDeviation={r * phi ** 3} />
 			<feDiffuseLighting
-				lighting-color={chroma.oklch(1, 0.37, 240).hex()}
-				surfaceScale={3}
-				diffuseConstant={3}
+				lighting-color={chroma.oklch(0.25, 0.37, 300).hex()}
+				surfaceScale={4}
+				diffuseConstant={1}
 				result="light"
 			>
-				<feDistantLight azimuth="-120" elevation="10" />
+				<feDistantLight azimuth="-90" elevation="10" />
 			</feDiffuseLighting>
 			<feComposite
 				in="SourceGraphic"
@@ -36,7 +36,7 @@
 		<BasicHexPattern
 			id="bhp"
 			{r}
-			fill={chroma.oklch(0.25, 0.17, 240).hex()}
+			fill={chroma.oklch(0.25, 0.185, 300).hex()}
 			filter="url(#bhp-filter)"
 		/>
 	</defs>
