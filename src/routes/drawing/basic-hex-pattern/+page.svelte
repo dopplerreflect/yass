db61a02dfa46874d12204a9f2088a8fa118d3cd2
David Rose <doppler@gmail.com>
2026-07-13 19:33:34 -0500

    bhp: golden hexes with glowy yellow flower of life grid

--- src/routes/drawing/basic-hex-pattern/+page.svelte
+++ src/routes/drawing/basic-hex-pattern/+page.svelte
@@ -7,55 +7,58 @@
 	import chroma from 'chroma-js';
 	const width = 1920;
 	const height = 1080;
-	const r = 1920 / 34;
+	const hexRadius = 1920 / 55;
 </script>
 
 <DrSvg {...{ width, height }}>
 	<defs>
 		<filter id="bhp-filter" x="-50%" y="-50%" width="300%" height="300%">
-			<feMorphology in="SourceGraphic" operator="erode" radius={r * phi ** 3} />
-			<feGaussianBlur stdDeviation={r * phi ** 3} />
+			<feMorphology in="SourceGraphic" operator="erode" radius={hexRadius * phi ** 5} />
+			<feGaussianBlur stdDeviation={hexRadius * phi ** 8} />
 			<feDiffuseLighting
-				lighting-color={chroma.oklch(0.5, 0.37, 300).hex()}
-				surfaceScale={4}
-				diffuseConstant={1.5}
+				lighting-color={chroma.oklch(0.95, 0.37, 90).hex()}
+				surfaceScale={2}
+				diffuseConstant={1.25}
 				result="light"
 			>
-				<feDistantLight azimuth="-90" elevation="10" />
+				<feDistantLight azimuth="-105" elevation="10" />
 			</feDiffuseLighting>
 			<feComposite
 				in="SourceGraphic"
 				in2="light"
 				operator="arithmetic"
-				k1={1.0}
-				k2={0.5}
+				k1={1.75}
+				k2={0.25}
 				k3={0}
 				k4={0}
 			/>
 		</filter>
 		<filter id="bhp-strokeFilter">
-			<feMorphology operator="dilate" radius={1} />
-			<feGaussianBlur stdDeviation={6} />
+			<feGaussianBlur stdDeviation={4} />
 			<feColorMatrix
 				values="
-					1.75 0 0 0 0
-					0 1.75 0 0 0
-					0 0 1.75 0 0
+					1 0 0 0 2
+					0 1 0 0 2
+					0 0 1 0 2
 					0 0 0 1 0
 				"
 			/>
 			<feMerge>
+				<feMergeNode />
+				<feMergeNode />
 				<feMergeNode />
 				<feMergeNode in="SourceGraphic" />
 			</feMerge>
 		</filter>
 		<BasicHexPattern
 			id="bhp"
-			{r}
-			fill={chroma.oklch(0.25, 0.37, 300).hex()}
+			{hexRadius}
+			hexFill={chroma.oklch(0.75, 0.37, 90).hex()}
 			fillFilter="url(#bhp-filter)"
-			strokeFilter="url(#bhp-strokeFilter)"
-			stroke={chroma.oklch(0.5, 0.185, 240).hex()}
+			circleStroke={chroma.oklch(1, 0.37, 90).hex()}
+			circleStrokeFilter="url(#bhp-strokeFilter)"
+			hexStroke={chroma.oklch(0.15, 0.185, 90).hex()}
+			hexStrokeWidth={4}
 		/>
 	</defs>
 	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="#300060" />
--- src/routes/drawing/basic-hex-pattern/BasicHexPattern.svelte
+++ src/routes/drawing/basic-hex-pattern/BasicHexPattern.svelte
@@ -1,36 +1,56 @@
 <svelte:options namespace="svg" />
 
 <script lang="ts">
-	import { anglesArray, radialPoint, radialPointString } from '@dopplerreflect/geometry';
+	import {
+		anglesArray,
+		phi,
+		radialPoint,
+		radialPointString,
+		type Circle,
+	} from '@dopplerreflect/geometry';
 
 	type Props = {
 		id: string;
-		r: number;
-		fill?: string;
-		stroke?: string;
-		strokeWidth?: number;
+		hexRadius: number;
+		hexFill?: string;
 		fillFilter?: string;
-		strokeFilter?: string;
+		circleStroke?: string;
+		circleStrokeFilter?: string;
+		hexStroke?: string;
+		hexStrokeWidth?: number;
 	};
 
 	let {
 		id,
-		r,
-		fill = 'none',
-		stroke = 'none',
-		strokeWidth = 1,
+		hexRadius,
+		hexFill = 'none',
 		fillFilter = '',
-		strokeFilter = '',
+		circleStroke = 'none',
+		circleStrokeFilter = '',
+		hexStroke = '',
+		hexStrokeWidth = 1,
 	}: Props = $props();
 
-	const width = $derived(r * Math.sqrt(3));
-	const height = $derived(r * 3);
+	const width = $derived(hexRadius * Math.sqrt(3));
+	const height = $derived(hexRadius * 3);
 	const x = $derived(-width / 2);
-	const y = $derived(-r * 1.5);
+	const y = $derived(-hexRadius * 1.5);
 
 	const hexPoints = anglesArray(6, 30)
-		.map((a) => radialPointString(a, r))
+		.map((a) => radialPointString(a, hexRadius))
 		.join(' ');
+
+	const radii = [...Array(3).keys()].map((k) => hexRadius * Math.sqrt(3) * phi ** k).slice(0, 1);
+
+	const angles = anglesArray(6, 0);
+
+	const circles: Circle[] = $derived([
+		...radii.map((r) => ({ r, x: 0, y: 0 })),
+		...angles
+			.map((a) => radii.map((r) => ({ r, ...radialPoint(a, hexRadius * Math.sqrt(3)) })))
+			.flat(),
+		...[-hexRadius * 3, hexRadius * 3].map((y) => radii.map((r) => ({ r, x: 0, y }))).flat(),
+	]);
 </script>
 
 <pattern
@@ -42,28 +62,25 @@
 	<defs>
 		<path
 			id={`${id}-hex`}
-			d={`M0 ${y} 0 ${-r}ZM0 ${r} 0 ${r * 1.5}ZM${hexPoints}Z`}
+			d={`M0 ${y} 0 ${-hexRadius}ZM0 ${hexRadius} 0 ${hexRadius * 1.5}ZM${hexPoints}Z`}
 			fill="none"
-			{stroke}
-			stroke-width={strokeWidth}
+			stroke={hexStroke}
+			stroke-width={hexStrokeWidth}
 		/>
-		<path id={`${id}-fill`} d={`M${hexPoints}Z`} {fill} filter={fillFilter} />
+		<path id={`${id}-fill`} d={`M${hexPoints}Z`} fill={hexFill} filter={fillFilter} />
 		<g id={`${id}-fillg`}>
 			<use href={`#${id}-fill`} />
-			{#each [60, 120, 240, 300].map((a) => radialPointString(a, r * Math.sqrt(3))) as t}
+			{#each [60, 120, 240, 300].map((a) => radialPointString(a, hexRadius * Math.sqrt(3))) as t}
 				<use href={`#${id}-fill`} transform={`translate(${t})`} />
 			{/each}
 		</g>
-		<g id={`${id}-cg`} {stroke} filter={strokeFilter}>
-			<circle r={r * Math.sqrt(3)} fill="none" />
-			<circle r={r * Math.sqrt(3)} cy={-r * 3} fill="none" />
-			<circle r={r * Math.sqrt(3)} cy={r * 3} fill="none" />
-			{#each anglesArray(6, 0).map( (a) => ({ r: r * Math.sqrt(3), ...radialPoint(a, r * Math.sqrt(3)) }), ) as c, i}
-				<circle cx={c.x} cy={c.y} r={c.r} fill="none" />
+		<g id={`${id}-cg`} stroke={circleStroke} fill="none" filter={circleStrokeFilter}>
+			{#each circles as c}
+				<circle cx={c.x} cy={c.y} r={c.r} />
 			{/each}
 		</g>
 	</defs>
-	<use href={`#${id}-fillg`} />
-	<use display="none" href={`#${id}-hex`} />
+	<use display="block" href={`#${id}-fillg`} />
+	<use display="block" href={`#${id}-hex`} stroke-width={hexStrokeWidth} />
 	<use href={`#${id}-cg`} />
 </pattern>
