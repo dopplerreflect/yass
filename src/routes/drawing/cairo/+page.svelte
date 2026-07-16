14954cf9244a64cbd498f72b593cdabbfa58ea4a
David Rose <doppler@gmail.com>
2026-07-16 16:28:49 -0500

    cairo: add triangle/square tile lines

--- src/routes/drawing/cairo/+page.svelte
+++ src/routes/drawing/cairo/+page.svelte
@@ -2,13 +2,20 @@
 
 <script lang="ts">
 	import DrSvg from '$lib/components/DrSvg.svelte';
-	import { polygonPointString, radialPoint, type Point } from '@dopplerreflect/geometry';
+	import {
+		pointToString,
+		polygonPointString,
+		radialPoint,
+		type Point,
+	} from '@dopplerreflect/geometry';
 	import chroma from 'chroma-js';
 	const oklch = chroma.oklch;
 	const width = 1920;
 	const height = 1080;
-	const unit = 34;
+	const unit = 50;
 	const unitb = unit * Math.sqrt(3) - unit;
+	const vb = [-unit * Math.sqrt(3), -unit - unitb, unit * Math.sqrt(3) * 2, (unit + unitb) * 2];
+	const cl = unitb * Math.sqrt(3);
 	const points0: Point[] = [{ x: unitb / 2, y: 0 }];
 	points0.push(radialPoint(-60, unit, { center: points0[0] }));
 	points0.push(radialPoint(30, unit, { center: points0[1] }));
@@ -20,6 +27,69 @@
 	points1.push(radialPoint(-150, unit, { center: points1[1] }));
 	points1.push(radialPoint(-210, unit, { center: points1[2] }));
 	points1.push(radialPoint(60, unit, { center: points1[3] }));
+
+	const base = {
+		p0: { x: 0, y: -cl / 2 },
+		p1: { x: 0, y: cl / 2 },
+		p2: radialPoint(30, cl, { center: { x: 0, y: -cl / 2 } }),
+		p3: radialPoint(150, cl, { center: { x: 0, y: -cl / 2 } }),
+	};
+	const p = {
+		...base,
+		p4: radialPoint(240, cl, { center: base.p0 }),
+		p5: radialPoint(300, cl, { center: base.p0 }),
+		p6: radialPoint(300, cl, { center: base.p2 }),
+		p7: radialPoint(60, cl, { center: base.p2 }),
+		p8: radialPoint(60, cl, { center: base.p1 }),
+		p9: radialPoint(120, cl, { center: base.p1 }),
+		p10: radialPoint(120, cl, { center: base.p3 }),
+		p11: radialPoint(240, cl, { center: base.p3 }),
+	};
+	const s = Object.fromEntries(Object.entries(p).map(([k, v]) => [k, pointToString(v)])) as Record<
+		keyof typeof p,
+		string
+	>;
+
+	const ppath = [
+		`M${s.p0}`,
+		s.p1,
+		s.p2,
+		s.p0,
+		s.p3,
+		s.p1,
+		'M',
+		s.p4,
+		s.p0,
+		s.p5,
+		s.p6,
+		s.p2,
+		s.p7,
+		s.p8,
+		s.p1,
+		s.p9,
+		s.p10,
+		s.p3,
+		s.p11,
+		s.p4,
+		'M',
+		s.p2,
+		pointToString(radialPoint(0, cl, { center: p.p2 })),
+		'M',
+		s.p3,
+		pointToString(radialPoint(180, cl, { center: p.p3 })),
+	].join(' ');
+	const qpath = [
+		`M${s.p4}`,
+		s.p5,
+		`M${s.p6}`,
+		pointToString(radialPoint(270, cl / 2, { center: p.p6 })),
+		`M${s.p7}`,
+		pointToString(radialPoint(90, cl / 2, { center: p.p7 })),
+		`M${s.p10}`,
+		pointToString(radialPoint(90, cl / 2, { center: p.p10 })),
+		`M${s.p11}`,
+		pointToString(radialPoint(270, cl / 2, { center: p.p11 })),
+	].join(' ');
 </script>
 
 <DrSvg {...{ width, height }}>
@@ -27,11 +97,11 @@
 		id="pattern"
 		width={unit * Math.sqrt(3) * 2}
 		height={(unit + unitb) * 2}
-		viewBox={`${-unit * Math.sqrt(3)} ${-unit - unitb} ${unit * Math.sqrt(3) * 2} ${(unit + unitb) * 2}`}
+		viewBox={`${vb.join(' ')}`}
 		stroke="black"
 		fill="#ff000088"
 		patternUnits="userSpaceOnUse"
-		patternTransform={`rotate(30)`}
+		patternTransform={`translate(${-vb[0]} ${-vb[1]}) rotate(15)`}
 	>
 		<defs>
 			<linearGradient id="lg0">
@@ -42,19 +112,54 @@
 				<stop offset={0} stop-color={oklch(0.0, 0.0925, 300).hex()} />
 				<stop offset={1} stop-color={oklch(0.25, 0.37, 300).hex()} />
 			</linearGradient>
+			<filter id="glow">
+				<feMorphology operator="dilate" radius={1} />
+				<feGaussianBlur stdDeviation={5} />
+				<feMerge>
+					<feMergeNode />
+					<feMergeNode in="SourceGraphic" />
+				</feMerge>
+			</filter>
 		</defs>
-		<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="black" />
-		<path d={`M${-width / 2} 0H${width / 2}`} stroke="white" />
 		<g id="c" stroke="none">
 			<polygon id="p0" points={polygonPointString(points0)} fill="url(#lg0)" />
 			<use href="#p0" transform={`rotate(180)`} />
 			<polygon id="p1" points={polygonPointString(points1)} fill="url(#lg1)" />
 			<use href="#p1" transform={`rotate(180)`} />
 		</g>
-		<use href="#c" transform={`translate(${-unit * Math.sqrt(3)} ${-unit - unitb})`} />
-		<use href="#c" transform={`translate(${unit * Math.sqrt(3)} ${-unit - unitb})`} />
-		<use href="#c" transform={`translate(${-unit * Math.sqrt(3)} ${unit + unitb})`} />
-		<use href="#c" transform={`translate(${unit * Math.sqrt(3)} ${unit + unitb})`} />
+		<use
+			display="block"
+			href="#c"
+			transform={`translate(${-unit * Math.sqrt(3)} ${-unit - unitb})`}
+		/>
+		<use
+			display="block"
+			href="#c"
+			transform={`translate(${unit * Math.sqrt(3)} ${-unit - unitb})`}
+		/>
+		<use
+			display="block"
+			href="#c"
+			transform={`translate(${-unit * Math.sqrt(3)} ${unit + unitb})`}
+		/>
+		<use
+			display="block"
+			href="#c"
+			transform={`translate(${unit * Math.sqrt(3)} ${unit + unitb})`}
+		/>
+		<g stroke={oklch(0.5, 0.37, 300).hex()} fill="none" filter="url(#glow)">
+			<path d={ppath} />
+			<path d={qpath} />
+		</g>
 	</pattern>
-	<path d={`M${-width / 2} ${-height / 2}h${width}v${height}h${-width}Z`} fill="url(#pattern)" />
+	<path
+		display="block"
+		d={`M${-width / 2} ${-height / 2}h${width}v${height}h${-width}Z`}
+		fill="black"
+	/>
+	<path
+		display="block"
+		d={`M${-width / 2} ${-height / 2}h${width}v${height}h${-width}Z`}
+		fill="url(#pattern)"
+	/>
 </DrSvg>
