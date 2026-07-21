8a2311dbf82f9dfb77c5544d815351f94aeb506b
David Rose <doppler@gmail.com>
2026-07-21 13:54:01 -0500

    classy: use goldenCircles

--- src/routes/drawing/classy/+page.svelte
+++ src/routes/drawing/classy/+page.svelte
@@ -2,33 +2,18 @@
 
 <script lang="ts">
 	import DrSvg from '$lib/components/DrSvg.svelte';
-	import { phi } from '@dopplerreflect/geometry';
+	import { anglesArray, arrayMap, goldenCircles, phi } from '@dopplerreflect/geometry';
 	import { DRsvgObjectTracker } from './drsvg-object-tracker';
 	const width = 1920;
 	const height = 1080;
 
+	const baseRadius = height / 4;
+
+	const angles = anglesArray(6);
+	const radii = arrayMap(2, (n) => baseRadius * phi ** n);
 	const ot = new DRsvgObjectTracker();
 
-	ot.add([
-		{ x: -200, y: -200 },
-		{ x: 200, y: 200 },
-	]);
-	ot.add([
-		{ x: 200, y: 200 },
-		{ x: 0, y: 200 },
-	]);
-	ot.add([
-		{ x: 0, y: 200 },
-		{ x: 0, y: -200 },
-	]);
-	ot.add({ x: 0, y: 0, r: 200 });
-	ot.add({ x: 0, y: 0, r: 200 * phi });
-	ot.add({ x: -200, y: 0, r: 200 * phi ** 2 });
-	ot.add({ x: -200, y: 0, r: 200 });
-	ot.add([
-		{ x: -200, y: -200 },
-		{ x: 0, y: -200 },
-	]);
+	goldenCircles(radii, angles).forEach((c) => ot.add(c));
 
 	const lines = ot.lines;
 	const circles = ot.circles;
@@ -37,14 +22,15 @@
 </script>
 
 <DrSvg {...{ width, height }}>
-	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="black" />
+	<rect x={-width / 2} y={-height / 2} {...{ width, height }} fill="#202020" />
 	{#each lines as l}
 		<line x1={l[0].x} y1={l[0].y} x2={l[1].x} y2={l[1].y} stroke="white" />
 	{/each}
 	{#each circles as c}
 		<circle cx={c.x} cy={c.y} r={c.r} stroke="white" fill="none" />
 	{/each}
-	{#each points as p}
+	{#each points as p, i}
 		<circle cx={p.x} cy={p.y} r={3} fill="yellow" />
+		<text x={p.x} y={p.y} fill="yellow">{i}</text>
 	{/each}
 </DrSvg>
