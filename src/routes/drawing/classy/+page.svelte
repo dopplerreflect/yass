98c5c0626a0c5a0d0c72096f48de1efdf770a131
David Rose <doppler@gmail.com>
2026-07-22 23:48:58 -0500

    classy: start drawing paths

--- src/routes/drawing/classy/+page.svelte
+++ src/routes/drawing/classy/+page.svelte
@@ -2,7 +2,15 @@
 
 <script lang="ts">
 	import DrSvg from '$lib/components/DrSvg.svelte';
-	import { anglesArray, arrayMap, goldenCircles, phi, type Line } from '@dopplerreflect/geometry';
+	import {
+		anglesArray,
+		arrayMap,
+		goldenCircles,
+		phi,
+		pointToString,
+		type Line,
+		type Point,
+	} from '@dopplerreflect/geometry';
 	import { rotateLineAroundOrigin } from './rotateLineAroundOrigin';
 	import { DRsvgObjectTracker } from './drsvg-object-tracker';
 	const width = 1080;
@@ -46,6 +54,92 @@
 		[points().p223, points().p188],
 		[points().p218, points().p41],
 	]);
+
+	type PathSegments = (string | Point)[];
+	function segmentsToPath(segments: PathSegments) {
+		return segments.map((s) => (typeof s === 'string' ? s : pointToString(s))).join(' ');
+	}
+
+	const path1Segments: PathSegments = [
+		'M',
+		points().p155,
+		points().p222,
+		points().p21,
+		points().p390,
+		points().p187,
+		points().p156,
+		points().p193,
+		points().p420,
+		points().p123,
+		points().p219,
+		'Z',
+		'M',
+		points().p218,
+		points().p350,
+		points().p293,
+		points().p163,
+		'Z',
+		'M',
+		points().p188,
+		points().p565,
+		points().p41,
+		points().p13,
+		'Z',
+		'M',
+		points().p366,
+		points().p223,
+		points().p157,
+		points().p260,
+		'Z',
+	];
+	const path2Segments: PathSegments = [
+		'M',
+		points().p193,
+		points().p188,
+		points().p13,
+		points().p149,
+		points().p115,
+		points().p154,
+		points().p207,
+		points().p244,
+		points().p163,
+		points().p293,
+		'Z',
+		'M',
+		points().p218,
+		points().p123,
+		points().p420,
+		points().p193,
+		points().p350,
+		'Z',
+		'M',
+		points().p156,
+		points().p187,
+		points().p41,
+		points().p565,
+		'Z',
+		'M',
+		points().p20,
+		points().p38,
+		points().p64,
+		points().p90,
+		'Z',
+		'M',
+		points().p537,
+		points().p112,
+		points().p183,
+		points().p162,
+		'Z',
+		'M',
+		points().p410,
+		points().p128,
+		points().p215,
+		points().p351,
+		points().p207,
+		'Z',
+	];
+	const path1 = segmentsToPath(path1Segments);
+	const path2 = segmentsToPath(path2Segments);
 </script>
 
 <DrSvg {...{ width, height }}>
@@ -81,4 +175,6 @@
 			<text x={p.x} y={p.y} font-size="0.5em">{k}</text>
 		{/each}
 	</g>
+	<path d={path1} fill="#ff000040" />
+	<path d={path2} fill="#0000ff40" />
 </DrSvg>
